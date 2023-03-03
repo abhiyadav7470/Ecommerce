56561e7e502f53e34972f38656a529453f9aba52
abhiyadav7470 <108442826+abhiyadav7470@users.noreply.github.com>
2023-03-03 17:15:12 +0530

    updates

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import {React} from 'react';
+import { Route, Routes, Router} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Header from './components/nav/Header';
+
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+      <Route exact path='/login' element={<Login/>}/>
+      <Route exact path='/register' element={<Register/>}/>
+    </Routes>
+    
+    </>
+  );
+}
+
+export default App;
